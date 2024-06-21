43783fa3b26b914f70e499023a5db932ace5d3b3
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-06-20 21:51:04 -0700

    Edited the session.js file

---
+++ backend/routes/api/session.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+// Log in
+router.post(
+    '/',
+    async (req, res, next) => {
+      const { credential, password } = req.body;
+  
+      const user = await User.unscoped().findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+  
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = { credential: 'No user with that username/email exists.' };
+        return next(err);
+      }
+
+      if (!bcrypt.compareSync(password, user.hashedPassword.toString())) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = { credential: 'The provided password is invalid' };
+        return next(err);
+      }
+  
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+  
+      await setTokenCookie(res, safeUser);
+  
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
+
+module.exports = router;
