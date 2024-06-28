b33b8992145ca3eb4be2dbd43f751853a3a96b12
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-06-27 21:21:25 -0700

    Fixed item migration - added image column

--- backend/db/migrations/20240627021454-create-item.js
+++ backend/db/migrations/20240627021454-create-item.js
@@ -41,6 +41,10 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
+      image: {
+        type: Sequelize.String,
+        allowNull: true,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
