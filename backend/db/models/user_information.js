a06167aff5e072954d3a63e7d790b3b513e6ac55
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-06-29 16:21:48 -0700

    Tired for real

--- backend/db/models/user_information.js
+++ backend/db/models/user_information.js
@@ -13,7 +13,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User_Information.belongsTo(models.User)
+      User_Information.belongsTo(models.User< {
+        foreignKey: {
+          name: 'user_id'
+        },
+        targetKey: 'id'
+      })
 
       User_Information.belongsTo(models.Order, {
         foreignKey: 'user_info'
