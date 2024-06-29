93789578461765698cf7a63052bbd9238f577c5b
Asazay Luvaiku <AsazayLuvaiku@gmail.com>
2024-06-29 13:16:13 -0700

    Trying...

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.belongsTo(models.User, {
-        foreignKey: 'user_id',
+        foreignKey: 'id',
         onDelete: 'CASCADE'
       });
 
